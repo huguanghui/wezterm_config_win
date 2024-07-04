699092c974c7cad99cbc9cf35251e6dda9a7e912
hugh <huguanghui1971@gmail.com>
2024-07-04 09:08:08 +0800

    update

--- events/tab-title.lua
+++ events/tab-title.lua
@@ -1,8 +1,18 @@
 local wezterm = require('wezterm')
 
+local nf = wezterm.nerdfonts
+
+local GLYPH_SEMI_CIRCLE_LEFT = nf.ple_left_half_circle_thick
+local GLYPH_SEMI_CIRCLE_RIGHT = nf.ple_right_half_circle_thick
+local GLYPH_CIRCLE = nf.fa_circle
+local GLYPH_ADMIN = nf.md_shield_half_full
+
 local M = {}
 
+local __cells__ = {}
+
 M.setup = function()
+
 
 end
 
