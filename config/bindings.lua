45fe8121bcea0d39bb26115e22ec430578de486b
hugh <huguanghui1971@gmail.com>
2024-07-04 23:58:52 +0800

    update

--- config/bindings.lua
+++ config/bindings.lua
@@ -14,13 +14,85 @@ elseif platform.is_win or platform.is_linux then
 end
 
 local keys = {
-  { key = 'F2',  mods = 'NONE',        action = act.ActivateCommandPalette },
-  { key = 'F3',  mods = 'NONE',        action = act.ShowLauncher },
-  { key = 'F4',  mods = 'NONE',        action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
-  { key = 'F11', mods = 'NONE',        action = act.ToggleFullScreen },
+  { key = 'F1',         mods = 'NONE',        action = 'ActivateCopyMode' },
+  { key = 'F2',         mods = 'NONE',        action = act.ActivateCommandPalette },
+  { key = 'F3',         mods = 'NONE',        action = act.ShowLauncher },
+  { key = 'F4',         mods = 'NONE',        action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
+  { key = 'F11',        mods = 'NONE',        action = act.ToggleFullScreen },
+  { key = 'f',          mods = mod.SUPER,     action = act.Search({ CaseInSensitiveString = '' }) },
+
+  -- cursor movement --
+  { key = 'LeftArrow',  mods = mod.SUPER,     action = act.SendString '\x1bOH' },
+  { key = 'RightArrow', mods = mod.SUPER,     action = act.SendString '\x1bOF' },
+  { key = 'Backspace',  mods = mod.SUPER,     action = act.SendString '\x15' },
+
+  -- copy/paste --
+  { key = 'c',          mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
+  { key = 'v',          mods = 'CTRL|SHIFT',  action = act.PasteFrom('Clipboard') },
 
   -- tabs --
-  { key = 'w',   mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
+  { key = 't',          mods = mod.SUPER,     action = act.SpawnTab('DefaultDomain') },
+  { key = 'w',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
+
+  -- tabs: navigation
+  { key = '[',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
+  { key = ']',          mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
+  { key = '[',          mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
+  { key = ']',          mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+
+  -- window --
+  -- spawn windows
+  { key = 'n',          mods = mod.SUPER,     action = act.SpawnWindow },
+
+  -- panes --
+  -- panes: split panes
+  {
+    key = [[\]],
+    mods = mod.SUPER,
+    action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+  },
+  {
+    key = [[\]],
+    mods = mod.SUPER_REV,
+    action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+  },
+
+  -- panes: zoom+close pane
+  { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
+  { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
+
+  -- panes: navigation
+  { key = 'k',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
+  { key = 'j',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Down') },
+  { key = 'h',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Left') },
+  { key = 'l',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Right') },
+  {
+    key = 'p',
+    mods = mod.SUPER_REV,
+    action = act.PaneSelect({ alphabet = '1234567890', mode = 'SwapWithActiveKeepFocus' }),
+  },
+
+  -- key-tables --
+  -- resizes fonts
+  {
+    key = 'f',
+    mods = 'LEADER',
+    action = act.ActivateKeyTable({
+      name = 'resize_font',
+      one_shot = false,
+      timemout_miliseconds = 1000,
+    }),
+  },
+  -- resize panes
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = act.ActivateKeyTable({
+      name = 'resize_pane',
+      one_shot = false,
+      timemout_miliseconds = 1000,
+    }),
+  },
 }
 
 -- local c = {}
@@ -60,8 +132,38 @@ local keys = {
 
 -- return c
 
+-- stylua: ignore
+local key_tables = {
+  resize_font = {
+    { key = 'k',      action = act.IncreaseFontSize },
+    { key = 'j',      action = act.DecreaseFontSize },
+    { key = 'r',      action = act.ResetFontSize },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'q',      action = 'PopKeyTable' },
+  },
+  resize_pane = {
+    { key = 'k',      action = act.AdjustPaneSize({ 'Up', 1 }) },
+    { key = 'j',      action = act.AdjustPaneSize({ 'Down', 1 }) },
+    { key = 'h',      action = act.AdjustPaneSize({ 'Left', 1 }) },
+    { key = 'l',      action = act.AdjustPaneSize({ 'Right', 1 }) },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'q',      action = 'PopKeyTable' },
+  },
+}
+
+local mouse_bindings = {
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
+}
+
 return {
   disable_default_key_bindings = true,
   leader = { key = 'Space', mods = mod.SUPER_REV },
   keys = keys,
+  key_tables = key_tables,
+  mouse_bindings = mouse_bindings,
 }
