d844258ac02cb8359ab459afcca27f5be496ec43
hugh <huguanghui1971@gmail.com>
2024-07-04 09:20:30 +0800

    update

--- events/tab-title.lua
+++ events/tab-title.lua
@@ -11,6 +11,17 @@ local M = {}
 
 local __cells__ = {}
 
+local colors = {
+  default = { bg = '#45475a', fg = '#1c1b19' },
+  is_active = { bg = '#7FB4CA', fg = '#11111b' },
+  hover = { bg = '#587d8c', fg = '#1c1b19' },
+}
+
+local _set_process_name = function(s)
+  local a = string.gsub(s, '(.*[/\\])(.*)', '%2')
+  return a:gsub('%.exe$', '')
+end
+
 M.setup = function()
 
 end
