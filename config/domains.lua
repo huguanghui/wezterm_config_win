6e9c5f01a6f285a684dbc2e8550c4ea6df6d75a7
hugh <huguanghui1971@gmail.com>
2024-07-05 09:11:13 +0800

    feat 支持串口配置

--- config/domains.lua
+++ config/domains.lua
@@ -7,4 +7,13 @@ return {
 
   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
   wsl_domains = {},
+
+  -- ref: https://wezfurlong.org/wezterm/config/lua/config/serial_ports.html
+  serial_ports = {
+    {
+      name = "s5",
+      port = 'COM5',
+      baud = 115200,
+    }
+  },
 }
