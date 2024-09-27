29b3e872ef44d161fe3f87a214faf27af622275a
hugh <huguanghui1971@gmail.com>
2024-09-27 09:10:31 +0800

    update(font)

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,10 +2,10 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 local font = 'JetBrainsMonoNerdFontCo NF'
-local font_size = platform().is_mac and 12 or 14
+local font_size = platform().is_mac and 12 or 12
 
 return {
-  font = wezterm.font(font),
+  font = wezterm.font(font, { weight = 'Bold', italic = true }),
   font_size = font_size,
 
   freetype_load_target = 'Normal',
