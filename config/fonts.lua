8b2558992cbf2968b92dcaa0e562d4d2c3ea6fa1
huguanghui <huguanghui1971@gmail.com>
2025-10-15 08:48:33 +0800

    fix 禁用缺失字形的警告
    命令字符会误报

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,12 +1,15 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrainsMonoNerdFontCo NF'
-local font_size = platform().is_mac and 12 or 12
+local font = 'JetBrainsMonoNFM Nerd Font'
+local font_size = platform().is_mac and 12 or 10
 
 return {
-  font = wezterm.font(font, { weight = 'Bold', italic = true }),
+  font = wezterm.font(font, { weight = 'Bold', italic = false }),
   font_size = font_size,
+
+  -- 禁用缺失字形的警告
+  warn_about_missing_glyphs = false,
 
   freetype_load_target = 'Normal',
   freetype_render_target = 'Normal',
