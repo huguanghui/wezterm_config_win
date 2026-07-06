43c1001b6ce78e0559fd9a5bad6464c043c5fced
hugh <huguanghui1971@gmail.com>
2026-07-06 09:58:54 +0800

    fix: 适配终端字体大小

--- events/dpi-font.lua
+++ events/dpi-font.lua
@@ -8,18 +8,20 @@ M.setup = function()
 
       local dims = window:get_dimensions()
       local dpi = dims.dpi or 96
+      -- wezterm.log_info('dpi-font: window-config-reloaded triggered', 'dpi=', dpi, 'dims=', dims)
 
       local font_size = 10.0
       if dpi >= 192 then
          font_size = 10.0
       elseif dpi >= 144 then
+         font_size = 11
       elseif dpi >= 120 then
-         font_size = 12.5
-         font_size = 12.5
+         font_size = 11.5
       else
          font_size = 14
       end
 
+      -- wezterm.log_info('dpi-font: setting override font_size =', font_size)
       overrides.font_size = font_size
       window:set_config_overrides(overrides)
    end)
